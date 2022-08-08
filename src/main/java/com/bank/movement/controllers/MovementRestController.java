55c9bb92bb38216c1364545c7e56c906efd73569
Adrian Norvic Melgarejo Siguas <amelgars@emeal.nttdata.com>
2022-08-08 17:01:39 -0500

    added logs and port

--- src/main/java/com/bank/movement/controllers/MovementRestController.java
+++ src/main/java/com/bank/movement/controllers/MovementRestController.java
@@ -22,35 +22,41 @@ public class MovementRestController
     @GetMapping
     public Mono<ResponseEntity<Object>> findAll()
     {
+        log.info("[INI] findAll Movement");
         return dao.findAll()
                 .doOnNext(movement -> log.info(movement.toString()))
                 .collectList()
                 .map(movements -> ResponseHandler.response("Done", HttpStatus.OK, movements))
-                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)));
+                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)))
+                .doFinally(fin -> log.info("[END] findAll Movement"));
     }
 
     @GetMapping("/{id}")
     public Mono<ResponseEntity<Object>> find(@PathVariable String id)
     {
+        log.info("[INI] find Movement");
         return dao.findById(id)
                 .doOnNext(movement -> log.info(movement.toString()))
                 .map(movement -> ResponseHandler.response("Done", HttpStatus.OK, movement))
-                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)));
+                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)))
+                .doFinally(fin -> log.info("[END] find Movement"));
     }
 
     @PostMapping
     public Mono<ResponseEntity<Object>> create(@RequestBody Movement mov)
     {
-
+        log.info("[INI] create Movement");
         return dao.save(mov)
                 .doOnNext(movement -> log.info(movement.toString()))
                 .map(movement -> ResponseHandler.response("Done", HttpStatus.OK, movement)                )
-                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)));
+                .onErrorResume(error -> Mono.just(ResponseHandler.response(error.getMessage(), HttpStatus.BAD_REQUEST, null)))
+                .doFinally(fin -> log.info("[END] create Movement"));
     }
 
     @PutMapping("/{id}")
     public Mono<ResponseEntity<Object>> update(@PathVariable("id") String id, @RequestBody Movement mov)
     {
+        log.info("[INI] update Movement");
         return dao.existsById(id).flatMap(check -> {
             if (check)
                 return dao.save(mov)
@@ -60,12 +66,13 @@ public class MovementRestController
             else
                 return Mono.just(ResponseHandler.response("Not found", HttpStatus.NOT_FOUND, null));
 
-        });
+        }).doFinally(fin -> log.info("[END] update Movement"));
     }
 
     @DeleteMapping("/{id}")
     public Mono<ResponseEntity<Object>> delete(@PathVariable("id") String id)
     {
+        log.info("[INI] delete Movement");
         log.info(id);
 
         return dao.existsById(id).flatMap(check -> {
@@ -73,6 +80,6 @@ public class MovementRestController
                 return dao.deleteById(id).then(Mono.just(ResponseHandler.response("Done", HttpStatus.OK, null)));
             else
                 return Mono.just(ResponseHandler.response("Not found", HttpStatus.NOT_FOUND, null));
-        });
+        }).doFinally(fin -> log.info("[END] delete Movement"));
     }
 }
