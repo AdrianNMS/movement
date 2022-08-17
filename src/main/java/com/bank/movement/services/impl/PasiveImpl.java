7bb1038e2edeb48ecd055c815442b9403662d5f1
AdrianNMS <adrian.n.melgarejo.s@gmail.com>
2022-08-17 17:28:55 -0500

    Added resilience4j library rename ParameterImpl file

--- src/main/java/com/bank/movement/services/impl/PasiveImpl.java
+++ src/main/java/com/bank/movement/services/impl/PasiveImpl.java
@@ -11,7 +11,7 @@ import reactor.core.publisher.Mono;
 
 
 @Service
-public class ParameterImpl implements IPasiveService {
+public class PasiveImpl implements IPasiveService {
 
     @Autowired
     WebClient webClient;
